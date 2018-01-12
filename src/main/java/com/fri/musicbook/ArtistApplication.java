6710681f31100d4a0299b32378898e9f15b90d75
Jan Blatnik <jb4634@student.uni-lj.si>
2018-01-12 20:24:58 +0100

    pred popravilom

--- src/main/java/com/fri/musicbook/ArtistApplication.java
+++ src/main/java/com/fri/musicbook/ArtistApplication.java
@@ -1,9 +1,11 @@
-package com.fri.musicbook;//import com.kumuluz.ee.discovery.annotations.RegisterService;
+package com.fri.musicbook;
 
+
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
-//@RegisterService
+@RegisterService
 public class ArtistApplication extends Application {
 }
